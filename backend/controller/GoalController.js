883a256ef1ef815c4db06d617397e7f38e00d4c4
Ramoeletsi <reanetse.ramoeletsi@bothouniversity.com>
2022-09-20 07:02:33 +0200

    Rest Api for Goals

--- backend/controller/GoalController.js
+++ backend/controller/GoalController.js
@@ -3,14 +3,11 @@ const Goal = require('../model/goalmodel')
 
 const getGoals = asyncHandler (async(req, res) => {
     const goals =await Goal.find()
-    // res.status(200).json({message:'Get goals'})
     res.status(200).json(goals)
 })
 
 const setGoal = asyncHandler (async(req, res) => {
-    // console.log(req.body)
     if(!req.body.text){
-        // res.status(400).json({message:'Please add a text field' })
         res.status(400)
         throw new Error('Please add a text field')
                
@@ -18,7 +15,6 @@ const setGoal = asyncHandler (async(req, res) => {
     const goal = await Goal.create({
         text: req.body.text
     })
-    // res.status(200).json({message:'Set goal'})
     res.status(200).json(goal)
 })
 
@@ -30,7 +26,6 @@ const updateGoal = asyncHandler (async(req, res) => {
         throw new Error('Goal not found')
     }
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new:true})
-    // res.status(200).json({message:` Update goal ${req.params.id}`})
     res.status(200).json(updatedGoal)
 })
 
@@ -42,7 +37,6 @@ const deleteGoal = asyncHandler (async (req, res) => {
         throw new Error('Goal not found')
     }
     await goals.remove()
-    // res.status(200).json({message:` Delete goal ${req.params.id}`})
     res.status(200).json({id: req.params.id})
 })
 
--- backend/routes/GoalRoutes.js
+++ backend/routes/GoalRoutes.js
@@ -2,22 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controller/GoalController')
 
-// changed (app to router ) 
-// changed ('/api/goals' to '/') to hide from the fron end and directed it to the json file
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
-// router.get('/', (req, res) => {
-//     // res.send('Get goals')
-//     res.status(200).json({message:'Get goals'})
-// })
-
-// router.route('/').get(getGoals).post(setGoal)
-// router.route('/:id').put(updateGoal).delete(deleteGoal)
-
-router.get('/', getGoals)
-
-router.post('/',setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id',deleteGoal)
 module.exports = router
